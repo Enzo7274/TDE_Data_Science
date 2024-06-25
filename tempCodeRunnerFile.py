4fa9c60684f8f44ef8fc1bf4b2d8c5b1f943e86d
Matheus <102936397+jma7th@users.noreply.github.com>
2024-06-24 23:51:04 -0300

    Delete tempCodeRunnerFile.py

--- tempCodeRunnerFile.py
+++
@@ -1,5 +0,0 @@
-    
-    # Sum values separated by lines containing different strings in the 'names' column
-    print("\nSum of values separated by unique strings in the 'names' column:\n")
-    sum_by_names = df.groupby('names').sum()
-    print(sum_by_names)
